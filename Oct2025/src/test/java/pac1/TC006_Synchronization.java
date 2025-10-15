88dfc6e54c6a4957a2df4a7cc735c634c9e92dec
Ascendion <Ascendion@10.0.2.15>
2025-10-15 09:55:32 +0530

    Lab6 and Lab8 done !!

---
+++ Oct2025/src/test/java/pac1/TC006_Synchronization.java
@@ -0,0 +1,33 @@
+package pac1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC006_Synchronization {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(3000));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		Thread.sleep(3000);
+		
+		WebElement ele = wait.until(ExpectedConditions.elementToBeClickable(By.name("username")));	
+		ele.sendKeys("Ishant");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.name("password")).sendKeys("Ishant@1234");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+	}
+
+}
